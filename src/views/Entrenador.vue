33e0dc842771727840ccf06965f39332636b3f13
isaacdaw2 <taveras1989@hotmail.com>
2020-04-17 14:21:55 +0200

    partes de entrenador

--- src/views/Entrenador.vue
+++ src/views/Entrenador.vue
@@ -4,15 +4,33 @@
         <h2>Envíanoslo y dirige a alguno de nuestros equipos</h2>
         <img src="../assets/entrenador.png">
 
+        <!-- Seleccionar archivo -->
+        <b-row class="mt-3">
+            <b-form-file
+                v-model="file"
+                :state="Boolean(file)"
+                placeholder="Elija un archivo o desplácelo aquí..."
+                drop-placeholder="Soltar archivo aquí..."
+                browse-text="Elegir"
+            ></b-form-file>
+        </b-row>
 
-
-        
-        
+        <!-- Botón de enviar-->
+        <b-row class="mt-3">
+            <b-col>
+                <b-button variant="success">Enviar</b-button>
+            </b-col>
+        </b-row>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'Entrenador'
+    name: 'Entrenador',
+    data() {
+      return {
+        file: null
+      }
+    }
 }
 </script>
