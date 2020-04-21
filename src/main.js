bda7e2d9d2a35ce283cacffa766102a912f3dfb2
Isaac Taveras <you@example.com>
2020-04-21 19:22:42 +0200

    mas cosas de noticias

--- src/main.js
+++ src/main.js
@@ -3,10 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import { BootstrapVue, IconsPlugin, ImagePlugin } from 'bootstrap-vue'
+import { BootstrapVue, IconsPlugin, ImagePlugin, CarouselPlugin } from 'bootstrap-vue'
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 Vue.use(ImagePlugin)
+Vue.use(CarouselPlugin)
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
--- src/views/Noticias.vue
+++ src/views/Noticias.vue
@@ -1,41 +1,117 @@
 <template>
-  <div>
-  <b-carousel
-    id="carousel-fade"
-    style="text-shadow: 0px 0px 2px #000"
-    fade
-    indicators
-    img-width="1024"
-    img-height="480"
-  >
-    <b-carousel-slide
-      caption="Bienvenid@ a nuestra web"
-      :img-src="require('../assets/balon-de-futbol.jpg')"
-    ></b-carousel-slide>
-    <b-carousel-slide
-      caption="Comparte nuestra pasión"
-      :img-src="require('../assets/campo.jpg')"
-    ></b-carousel-slide>
-    <b-carousel-slide
-      caption="Aquí encontrarás las noticias del club"
-      :img-src="require('../assets/escuela.jpg')"
-    ></b-carousel-slide>
-    <b-carousel-slide
-      caption=""
-      :img-src="require('../assets/futbol.jpg')"
-    ></b-carousel-slide>
-  </b-carousel>
-</div>
+  <div class="mt-3">
+
+    <!-- Cabecera slides -->
+    <b-carousel
+        id="carousel-fade"
+        style="text-shadow: 0px 0px 2px #000"
+        fade="fade"
+        indicators="indicators"
+        img-width="1024"
+        img-height="480">
+        <b-carousel-slide
+            caption="Bienvenid@ a nuestra web"
+            :img-src="require('../assets/balon-de-futbol.jpg')"></b-carousel-slide>
+        <b-carousel-slide
+            caption="Comparte nuestra pasión"
+            :img-src="require('../assets/campo.jpg')"></b-carousel-slide>
+        <b-carousel-slide
+            caption="Aquí encontrarás las noticias del club"
+            :img-src="require('../assets/escuela.jpg')"></b-carousel-slide>
+        <b-carousel-slide
+            caption="Resultados"
+            :img-src="require('../assets/futbol.jpg')"></b-carousel-slide>
+    </b-carousel>
+
+      <!-- cards con noticias -->
+    <div class="mt-3 mb-3">
+      <!-- Primera línea de cards -->
+      <div>
+        <b-card-group deck="deck">
+            <b-card
+                bg-variant="primary"
+                text-variant="white"
+                header="Instalaciones"
+                class="text-center">
+                <b-card-text>Conoce nuestras instalaciones</b-card-text>
+            </b-card>
+
+            <b-card
+                bg-variant="secondary"
+                text-variant="white"
+                header="Plantillas"
+                class="text-center">
+                <b-card-text>Conoce nuestras plantillas</b-card-text>
+            </b-card>
+
+            <b-card
+                bg-variant="success"
+                text-variant="white"
+                header="Resultados"
+                class="text-center">
+                <b-card-text>Mira como nos ha ido en los últimos partidos</b-card-text>
+            </b-card>
+        </b-card-group>
+      </div>
+
+      <!-- Segunda línea de cards -->
+      <div class="mt-3">
+        <b-card-group deck="deck">
+            <b-card
+                bg-variant="info"
+                text-variant="white"
+                header="Clasificación"
+                class="text-center">
+                <b-card-text>Observa nuestra clasificación en la tabla</b-card-text>
+            </b-card>
+
+            <b-card
+                bg-variant="warning"
+                text-variant="white"
+                header="Warning"
+                class="text-center">
+                <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+            </b-card>
+
+            <b-card
+                bg-variant="danger"
+                text-variant="white"
+                header="Danger"
+                class="text-center">
+                <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+            </b-card>
+        </b-card-group>
+      </div>
+
+      <!-- Tercera línea de cards -->                      
+      <div class="mt-3">
+        <b-card-group deck="deck">
+            <b-card bg-variant="light" header="Light" class="text-center">
+                <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+            </b-card>
+
+            <b-card
+                bg-variant="dark"
+                header="Dark"
+                text-variant="white"
+                class="text-center">
+                <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+            </b-card>
+
+            <b-card header="Default" class="text-center">
+                <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+            </b-card>
+        </b-card-group>
+      </div>
+    </div>
+  </div>    
 </template>
 
 <script>
-// @ is an alias to /src
-import Noticias from '@/components/Noticias.vue'
+    // @ is an alias to /src
+    import Noticias from '@/components/Noticias.vue'
 
-export default {
-  name: 'Home',
-  components: {
-    Noticias
-  }
-}
+    export default {name: 'Home', components: {
+            Noticias
+        }}
 </script>
