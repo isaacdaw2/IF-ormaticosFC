5764af27bc67164afa2c584caa05643352f70bb8
Isaac Taveras <you@example.com>
2020-04-25 20:49:50 +0200

    icono del nav bien

--- src/App.vue
+++ src/App.vue
@@ -8,10 +8,9 @@
           <b-navbar-toggle target="navbar-toggle-collapse">
             <template v-slot:default="{ expanded }">
               <b-icon v-if="expanded" icon="chevron-bar-up"></b-icon>
-              <b-icon v-else="v-else" icon="chevron-bar-down"></b-icon>
+              <b-icon v-else icon="chevron-bar-down"></b-icon>
             </template>
           </b-navbar-toggle>
-          
           <b-navbar-brand :to="{name: 'Noticias'}">
             <img src="./assets/balon.png" alt="" width="40">
             IF-ormáticos FC</b-navbar-brand>
